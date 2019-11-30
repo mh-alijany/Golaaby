0dfb5a34126414d4baf675b405ec12cdf89f500e
alijany <mh.alijany@gamil.com>
2019-11-30 21:00:26 +0330

    add update  table row

--- src/nsLookup.js
+++ src/nsLookup.js
@@ -7,12 +7,14 @@ function setDNS_Servers(DNS_array) {
 }
 
 // measure time of resolving a domain
-export async function measure(DNS_array, dist = 'google.com') {
-    setDNS_Servers(DNS_array);
-    var time = new Date().getTime();
-    resolver.resolve4(dist, (err, addresses) => {
-        let result;
-        result = (!err && addresses) ? new Date().getTime() - time : 'نا موجود';
-        return new Promise(resolve => resolve(result));
+export function measure(DNS_array, dist = 'google.com') {
+    return new Promise(resolve => {
+        setDNS_Servers(DNS_array);
+        var time = new Date().getTime();
+        resolver.resolve4(dist, (err, addresses) => {
+            let result;
+            result = (!err && addresses) ? new Date().getTime() - time : 'نا موجود';
+            resolve(result)
+        });
     });
 }
--- src/renderer.js
+++ src/renderer.js
@@ -3,17 +3,16 @@ import './main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 var { store, data } = require('./fs');
-require('./nsLookup');
+var { measure } = require('./nsLookup');
 
 // init user interface ----------------------------------------------
 
 // create a table row for a DNS
 function makeRow(dns) {
-    var row = $(`<tr><td>options</td><td><span class="badge badge-warning">درحال برسی</span></td>
-            <td>درحال برسی</td><td>${dns.name}</td></tr>`);
+    var row = $(`<tr><td>options</td><td><span class="badge badge-warning">درحال برسی</span></td><td>${dns.name}</td></tr>`);
 
     // attach dns data to row
-    row.data('data', { id: dns.name, DNS1: dns.DNS1, DNS2: dns.DNS2 });
+    row.data('data', { id: dns.name, DNS_servers: [dns.DNS1, dns.DNS2] });
     return row;
 }
 
@@ -26,7 +25,22 @@ function addDNSToTable() {
     $("#DNS-table").append(tableBody);
 }
 
-addDNSToTable()
+async function updateDNS_State() {
+    var rows = $("#DNS-table tr")
+
+    for (let i = 0; i < rows.length; i++) {
+        var dns_servers = $(rows[i]).data('data').DNS_servers;
+        var latency = await measure(dns_servers);
+        if (latency) {
+            $(rows[i]).children().eq(1).html(`<span class="badge badge-success">${latency}</span>`)
+        } else
+            $(rows[i]).children().eq(1).html('<span class="badge badge-warn">قطع</span>')
+    }
+}
+
+addDNSToTable();
+updateDNS_State()
+
 
 // const { remote } = require('electron');
 // const { BrowserWindow } = remote;
