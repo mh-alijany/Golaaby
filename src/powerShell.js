3d0d7e6fc956f8946f406f8901e856e2f3e3ae75
alijany <mh.alijany@gamil.com>
2019-12-01 23:22:50 +0330

    export function

--- src/powerShell.js
+++ src/powerShell.js
@@ -5,24 +5,18 @@ let ps = new shell({
     noProfile: true
 });
 
-// Get-NetAdapter | Select-Object  InterfaceAlias , InterfaceIndex , Status | ConvertTo-Json
-// Set-DnsClientServerAddress -InterfaceIndex 5 -ResetServerAddresses
-// Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses ("10.0.0.1","10.0.0.2")
 
-// ps.addCommand('')
-// ps.invoke()
-
-async function getNetworks() {
+export function getNetworks() {
     ps.addCommand('Get-NetAdapter | Select-Object  InterfaceAlias , InterfaceIndex , Status | ConvertTo-Json');
     return ps.invoke();
 }
 
-function setDNS_servers(index, DNS1, DNS2) {
+export function setDNS_servers(index, DNS1, DNS2) {
     ps.addCommand(`Set-DnsClientServerAddress -InterfaceIndex ${index} -ServerAddresses ("${DNS1}","${DNS2}")`);
     return ps.invoke();
 }
 
-function setDNS_Auto(index) {
+export function setDNS_Auto(index) {
     ps.addCommand(`Set-DnsClientServerAddress -InterfaceIndex ${index} -ResetServerAddresses`);
     return ps.invoke();
 }
