4c66fb4472362cef3f89f83b37633a0326cbcdee
alijany <mh.alijany@gamil.com>
2019-12-02 11:56:45 +0330

    normalize data structure

--- src/fs.js
+++ src/fs.js
@@ -6,20 +6,22 @@ const filePath = path.resolve('src/data.json');
 
 export var data = {
     // default dns list
-    DNS_list: [
-        {
+    DNS_list: {
+        "1": {
+            id: 1,
             name: 'شکن',
             DNS_servers: ['178.22.122.100', '185.51.200.2'],
             icon: '',
             url: 'https://shecan.ir/'
         },
-        {
+        "2": {
+            id: 2,
             name: 'بگذر',
             DNS_servers: ['185.55.226.26', '185.55.226.25'],
             icon: '',
             url: 'https://begzar.ir/'
         }
-    ]
+    }
 };
 
 /**
