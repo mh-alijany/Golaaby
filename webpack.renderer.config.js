2203a28895385fb92a414333e77ce4da8644d493
alijany <mh.alijany@gamil.com>
2019-12-08 12:56:44 +0330

    Add react to project

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,10 +1,20 @@
 const rules = require('./webpack.rules');
 const webpack = require('webpack');
+const babelConfig = require('./babel.config.js');
 
 rules.push({
   test: /\.css|.scss$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
 });
+
+rules.push({
+  test: /\.(js|jsx)$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: babelConfig
+  }
+})
 
 module.exports = {
   // Put your normal webpack config below here
@@ -14,6 +24,7 @@ module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
       "$": 'jquery',
+      'React' : 'react'
     })
   ],
 };
