4a676c0fa2feebb162855d0df0b63c948607d6e6
alijany <mh.alijany@gamil.com>
2019-12-11 14:15:27 +0330

    convert to class syntac

--- src/App/components/ManualTab/ManualTab.js
+++ src/App/components/ManualTab/ManualTab.js
@@ -1,40 +1,42 @@
 import DNS_Form from './components/DNS_Form';
 import DNS_Row from './components/DNS_Row';
 
-const ManualTab = (props) => {
-    var [DNS_Info, setDNS_Info, HasUpdate, setHasUpdate] = props.DNS_Info;
+export class ManualTab extends React.Component {
+    constructor(props) {
+        super(props);
+        // [DNS_Info, setDNS_Info, HasUpdate, setHasUpdate]
+        this.DNS_Info = props.DNS_Info[0];
+        this.rows = Object.values(this.DNS_Info.DNS_List).map((DNS) => <DNS_Row DNS={DNS} key={DNS.id} />);
+    }
 
-    var rows = Object.values(DNS_Info.DNS_List).map((DNS) => <DNS_Row DNS={DNS} key={DNS.id} />);
+    render() {
+        return (
+            <div className={"tab-pane fade show active " + (this.props.isActive ? '' : "d-none")}>
 
-    return (
-        <div className={"tab-pane fade show active " + (props.isActive ? '' : "d-none")}>
+                <div className="m-3 mt-5">
+                    <div className="d-flex mb-3 justify-content-between align-items-center">
+                        <button type="button" className="btn btn-primary mx-3 my-2 ">افزودن</button>
+                        <h6 className="mx-3 my-2 text-primary">ها DNS لیست</h6>
+                    </div>
 
-            <div className="m-3 mt-5 rounded">
-
-                <div className="d-flex mb-3 justify-content-between align-items-center">
-                    <button type="button" className="btn btn-primary mx-3 my-2 ">افزودن</button>
-                    <h6 className="mx-3 my-2 text-primary">ها DNS لیست</h6>
+                    <table className="table mb-0 table-hover text-right text-dark">
+                        <thead>
+                            <tr className="te">
+                                <td scope="col">گذینه ها</td>
+                                <td scope="col">تاخیر</td>
+                                <td scope="col">سرویس دهنده</td>
+                            </tr>
+                        </thead>
+                        <tbody id='DNS-table'>
+                            {this.rows}
+                        </tbody>
+                    </table>
                 </div>
 
-                <table className="table mb-0 table-hover text-right text-dark">
-                    <thead>
-                        <tr className="te">
-                            <td scope="col">گذینه ها</td>
-                            <td scope="col">تاخیر</td>
-                            <td scope="col">سرویس دهنده</td>
-                        </tr>
-                    </thead>
-
-                    <tbody id='DNS-table'>
-                        {rows}
-                    </tbody>
-                </table>
+                <DNS_Form title="تست" display={true} />
             </div>
-
-            <DNS_Form title="تست" display={true} />
-
-        </div>
-    );
+        );
+    }
 }
 
 export default ManualTab;
