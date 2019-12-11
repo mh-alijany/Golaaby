b3d5975615519a4d865fce1db0849b47edaa08b5
alijany <mh.alijany@gamil.com>
2019-12-11 22:20:32 +0330

    ADD IP VALIDATE INPUT

--- src/App/components/ManualTab/components/DNS_Form.js
+++ src/App/components/ManualTab/components/DNS_Form.js
@@ -18,12 +18,31 @@ class DNS_Form extends React.Component {
         })
     }
 
-    updateIP(event, index) {
-        if (index == 1)
-            this.setState({ DNS1: event.target.value });
-        else
-            this.setState({ DNS2: event.target.value });
+    updateIP(event, DNS) {
+        let ip = event.target.value || "";
 
+        if (this.state["DNS1"].indexOf(ip) === 0) {
+            this.setState({ DNS1: ip });
+            return;
+        }
+
+        let regex = /(\d{1,3})\.?/g;
+        ip = ip.match(regex) || [];
+        let _ip = "";
+
+        ip.forEach((part, index) => {
+            if (index > 3) return;
+
+            let dot = '';
+            if (index != 3 && (part > 100 || part.endsWith(".")))
+                dot = ".";
+
+            _ip += Math.min(255, part) + dot;
+        });
+
+        let newState = {};
+        newState[DNS] = _ip;
+        this.setState(newState);
     }
 
     exit(done) {
@@ -47,13 +66,13 @@ class DNS_Form extends React.Component {
                                 <div className="form-group row">
                                     <div className="col-6">
                                         <label htmlFor="DNS1" className="float-right">DNS ترجیحی :</label>
-                                        <input id="DNS1" value={this.state.DNS1} onChange={(e) => this.updateIP(e, 1)}
+                                        <input id="DNS1" value={this.state["DNS1"]} onChange={(e) => this.updateIP(e, "DNS1")}
                                             style={{ direction: "ltr" }} placeholder="DNS 1" type="text" className="form-control" />
                                     </div>
 
                                     <div className="col-6">
                                         <label htmlFor="DNS2" className="float-right">DNS جایگذین :</label>
-                                        <input id="DNS1" value={this.state.DNS2} onChange={(e) => this.updateIP(e, 2)}
+                                        <input id="DNS1" value={this.state["DNS2"]} onChange={(e) => this.updateIP(e, "DNS2")}
                                             style={{ direction: "ltr" }} placeholder="DNS 2" type="text" className="form-control" />
                                     </div>
                                 </div>
@@ -74,7 +93,7 @@ class DNS_Form extends React.Component {
                             <button type="button" onClick={() => this.exit(true)}
                                 className="btn btn-primary">قبول</button>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
---
+++ test.js
@@ -0,0 +1,3 @@
+let regex = /([0-9]{1,3})\.?/g;
+temp = "1.22."
+console.log(temp.match(regex));
