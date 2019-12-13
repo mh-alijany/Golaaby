9bdd43a62749f41c1813a6bfc3d5808ea20776e1
alijany <mh.alijany@gamil.com>
2019-12-13 08:35:09 +0330

    tabs style change while switch

--- src/App/components/Nav.js
+++ src/App/components/Nav.js
@@ -1,18 +1,20 @@
 class Nav extends React.Component {
-    constructor(props) {
-        super(props);
-        this.links = props.tabs_name.map((name, index) =>
-            <a id="pills-con" data-toggle="pill" aria-controls="tab-con" aria-selected="true"
-                className={name == props.defaultTab ? 'nav-link active' : 'nav-link'}
-                key={index}
-                onClick={() => props.setTab(name)}
-                role="tab">
-                {name}
-            </a>
-        )
+
+    shouldComponentUpdate(nextProps) {
+        return this.props.defaultTab != nextProps.defaultTab;
     }
 
     render() {
+        this.links = this.props.tabs_name.map((name, index) =>
+            <a id="pills-con" data-toggle="pill" aria-controls="tab-con" aria-selected="true"
+                className={name == this.props.defaultTab ? 'nav-link active' : 'nav-link'}
+                key={index}
+                onClick={() => this.props.setTab(name)}
+                role="tab">
+                {name}
+            </a>
+        );
+
         return (
             <div className="nav flex-column nav-pills text-right mt-5" id="v-pills-tab" role="tablist"
                 aria-orientation="vertical">
--- test.js
+++
@@ -1,3 +0,0 @@
-let regex = /([0-9]{1,3})\.?/g;
-temp = "1.22."
-console.log(temp.match(regex));
