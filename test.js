b8092f7b8c02c594176bb17886c057526e8d3d63
alijany <mh.alijany@gamil.com>
2019-12-14 13:08:42 +0330

    ignore test

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-let a = "123.32.43.43";
-let b = "123.32.43.43";
+var spawn = require("child_process").spawn, child;
 
+child = spawn("powershell.exe");
 
-console.log(.test(a));
+var get = false;
+child.stdout.on("data", function (data) {
+    get = true;
+    console.log("Powershell Data: " + data);
+});
 
+child.stderr.on("data", function (data) {
+    console.log("Powershell Errors: " + data);
+});
+
+child.on("exit", function () {
+    console.log("Powershell Script finished");
+});
+
+setInterval(() => {
+    if (get) {
+        get = false;
+        child.stdin.write("Get-Date\n");
+    }
+}, 1000);
