2f7d8f3162b73a6b1bacdc81dc3be9934f2695ee
alijany <mh.alijany@gamil.com>
2019-12-16 13:05:07 +0330

    extrade forge config

---
+++ forge.config.js
@@ -0,0 +1,41 @@
+const { WebpackPlugin } = require('@electron-forge/plugin-webpack')
+
+module.exports = {
+    electronPackagerConfig: {
+        icon: __dirname + "/build/icon.ico",
+        name: "golaaby"
+    },
+    packagerConfig: {
+        icon: __dirname + "/build/icon.ico",
+        name: "golaaby"
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "golaaby",
+                owners: "mohammad h alijany",
+                title: "golaaby",
+                noMsi: false,
+                icon: "./build/icon.ico",
+                setupIcon: "./build/icon.ico",
+                iconUrl: "https://cdn-24.anonfile.com/j1aasfGan9/6d39c050-1576483615/icon.ico"
+            }
+        }
+    ],
+    plugins: [
+        ["@electron-forge/plugin-webpack", {
+            mainConfig: "./webpack.main.config.js",
+            renderer: {
+                config: "./webpack.renderer.config.js",
+                entryPoints: [
+                    {
+                        html: "./src/index.html",
+                        js: "./src/renderer.js",
+                        name: "main_window"
+                    }
+                ]
+            }
+        }]
+    ]
+}
