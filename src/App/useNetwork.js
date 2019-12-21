8502b5381de016bb4d921b0b88f58db30ed39784
alijany <mh.alijany@gamil.com>
2019-12-21 23:33:12 +0330

    add comments

--- src/App/useNetwork.js
+++ src/App/useNetwork.js
@@ -1,17 +1,24 @@
 const os = require('os');
 
 const useNetwork = () => {
+    // state which represent Interface Changes by toggle
     var [InterfaceChang, setInterfaceChang] = React.useState(true);
+    // get connected interfaces of system
     let interfaces = Object.keys(os.networkInterfaces());
 
     React.useEffect(() => {
+        // check for network changes every 3 second
         var timer = setInterval(() => {
+            // get connected interfaces list
             let newInterfaces = Object.keys(os.networkInterfaces());
+            // if connected interface has changed
             if (newInterfaces.toString() != interfaces.toString()) {
                 setInterfaceChang(InterfaceChang => !InterfaceChang);
             }
+            // update interfaces
             interfaces = newInterfaces;
         }, 3000);
+
         return () => clearInterval(timer);
     }, []);
 
